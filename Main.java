fb6636ce8d497e47ae2135d28e54f4e331f1482c
KYU SEUNG SIM <simk11@uwgb.edu>
2020-07-24 04:02:38 +0000

    Array -> ArrayList

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.io.*;  // Import the File class
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.*; // Import the Scanner class to read text files
 
 class compSciList {
   public void compSciClassList() {
@@ -56,41 +56,9 @@ class commList extends compSciList {
 
 class enrollment {
 
-  public void addClass() {
-
-    /*  for(int userIndex = 0; userIndex < userClassList.length; userIndex += 0) {
-      System.out.println("EX) CS201");
-      System.out.print("Enter Your Class Name: ");
-
-      String userInput = user_input.nextLine();
-
-      if(totalCredit > 18) {
-        System.out.println("Total Credit can't over 18");
-        userIndex -= 1;
-        break;
-      } else if(userInput.equals("CS201")){  // Making checking list
-        userClassList[userIndex] = userInput;
-        userIndex += 1;
-        totalCredit += 3;
-        System.out.println("Success to Input!");
-      } else if(userInput.equals("CS221")){  // Making checking list
-        userClassList[userIndex] = userInput;
-        userIndex += 1;
-        totalCredit += 3;
-        System.out.println("Success to Input!");
-      } else if(userInput.equals("CS240")){  // Making checking list
-        userClassList[userIndex] = userInput;
-        userIndex += 1;
-        totalCredit += 4;
-        System.out.println("Success to Input!");
-      }
-
-    } // for loop end
-    */
+  public void matchClass() {
 
   }
-
-
 
   public void enrollmentStart() {
 
@@ -100,6 +68,7 @@ class enrollment {
     Scanner user_input = new Scanner(System.in);
     Scanner user_input_int = new Scanner(System.in);
     Scanner user_input_string = new Scanner(System.in);
+
 
     System.out.println("Welcome UWGB enrollment System");  
 
@@ -129,68 +98,38 @@ class enrollment {
       } else {
         System.out.println("Invalid Input!");
       }
-    }
- 
-    System.out.print("How many class will take this semester: ");
-    int user_input_class_list = user_input.nextInt();
-    String[] userClassList = new String[user_input_class_list];
-    System.out.println();    
+    } 
 
+    ArrayList<String> classList = new ArrayList<String>();
 
     while (i < 1000) {
     System.out.println("You can select to next thing");
-    System.out.println("1. ADD \n2. DROP \n3. Switch \n0. Finish");
+    System.out.println("1. ADD \n2. DROP \n0. Finish");
     System.out.println();
     System.out.print("Please enter the number to choose: ");
     int user_select_class_process = user_input_int.nextInt();  
     System.out.println();  
 
       if (user_select_class_process == 1) {
-        for(int userIndex = 0; userIndex < userClassList.length; userIndex += 0) {
-          System.out.println("EX) CS201 and Finish input 0");
-          System.out.print("Enter Your Class Name: ");
-
-          String userInput = user_input.nextLine();
-
-          if(totalCredit > 18) {
-            System.out.println("Total Credit can't over 18");
-            break;
-          } else if(userInput.equals("CS201")){  // Making checking list
-            userClassList[userIndex] = userInput;
-            userIndex += 1;
-            totalCredit += 3;
-            System.out.println("Success to Input!");
-          } else if(userInput.equals("CS221")){  // Making checking list
-            userClassList[userIndex] = userInput;
-            userIndex += 1;
-            totalCredit += 3;
-            System.out.println("Success to Input!");
-          } else if(userInput.equals("CS240")){  // Making checking list
-            userClassList[userIndex] = userInput;
-            userIndex += 1;
-            totalCredit += 4;
-            System.out.println("Success to Input!");
-          } else if(userInput.equals("0")){
-            break;
-          }
-
-        } // for loop end
-
+        if(totalCredit > 18){
+          System.out.println("Total Creadit Can't Over 18");
+        } else { 
+        System.out.println();
+        System.out.print("Which Class do you want to add: ");
+        String userInputAddClass = user_input.nextLine();
+        classList.add(userInputAddClass);
+        System.out.println();
         i++;
+        }
       } else if (user_select_class_process == 2) {
           System.out.print("Your Class List is: ");
-          for (int c = 0; c < userClassList.length; c++) {
-            System.out.print(userClassList[c] + ", ");
-          }
+          System.out.println(classList);
           System.out.println();
-
-
-
-        i++;
-      } else if (user_select_class_process == 3) {
-
-
-
+          System.out.println("It Strat with 0!");
+          System.out.print("Select Position to drop class: ");
+          int userInputDropClass = user_input.nextInt();
+          classList.remove(userInputDropClass);
+          System.out.println();
         i++;
       } else if (user_select_class_process == 0) {
         break; 
@@ -200,21 +139,11 @@ class enrollment {
     }
 
     System.out.print("Your Class List is: ");
-    
-    for (int c = 0; c < userClassList.length; c++) {
-      System.out.print(userClassList[c] + ", ");
-    }
+    System.out.println(classList);
     System.out.println();
     System.out.print("Your Total Creadit is " + totalCredit);
-
-
-
-
-
-
   }
 }
-
 
 class Main {
   public static void main(String[] args) {
